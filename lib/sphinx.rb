5be25c027201dddbd46d435d2bc95f5e3c59cf09
Bryce Frazier and Hannah Lee <bryce@>
2018-02-13 11:49:06 -0800

    3 passing tests

--- lib/sphinx.rb
+++ lib/sphinx.rb
@@ -6,10 +6,32 @@ class Riddle
   end
 
   def check
-    if (@answer1 == "a clock") && (@answer2 == "a candle") && (@answer3 == "a teapot")
-      "Correct"
-    elsif (@answer1 != "a clock") || (@answer2 != "a candle") || (@answer3 != "a teapot")
-      "Fail"
+    riddle_hash = {
+      "I’m tall when I’m young and I’m short when I’m old. What am I?" => "a candle",
+      "What has hands but can not clap?" => "a clock",
+      "What starts with the letter “t”, is filled with “t” and ends in “t”?" => "a teapot",
+      "You walk into a room with a match, a karosene lamp, a candle, and a fireplace. Which do you light first?" => "the match",
+      "What gets wetter and wetter the more it dries?" => "a towel",
+      "Name four days of the week that start with the letter “t”?" => "tuesday, thursday, today, and tomorrow."
+    }
+
+    input1 = @answer1.downcase
+    input2 = @answer2.downcase
+    input3 = @answer3.downcase
+
+    # if %w{input1 input2 input3}.any? {|input|} riddle_hash[:value] >= 3
+    #   "Success!"
+    # end
+    num_of_attempts = 0
+    until num_of_attempts == 3
+      if (input1 == "a clock") && (input2 == "a candle") && (input3 == "a teapot")
+        num_of_attempts += 1
+        return "Success!"
+      elsif (input1 != "a clock") || (input2 != "a candle") || (input3 != "a teapot")
+        num_of_attempts += 1
+        return "Fail!"
+      end
     end
   end
+
 end
--- spec/sphinx_spec.rb
+++ spec/sphinx_spec.rb
@@ -6,11 +6,16 @@ describe(Riddle) do
   describe('#check') do
     it("returns correct if the riddle answer is correct") do
       riddle1 = Riddle.new("a clock", "a candle", "a teapot")
-      expect(riddle1.check()).to(eq("Correct"))
+      expect(riddle1.check()).to(eq("Success!"))
     end
     it("returns fail if any answers are incorrect") do
       riddle1 = Riddle.new("a clock", "a candle", "nothing")
-      expect(riddle1.check()).to(eq("Fail"))
+      expect(riddle1.check()).to(eq("Fail!"))
     end
+    it("returns number of attempts once answers are provided") do
+      riddle1 = Riddle.new("a clock", "a candle", "nothing")
+      expect(riddle1.check()).to(eq("Fail!"))
+    end
+
   end
 end
